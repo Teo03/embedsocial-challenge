e0d9f4c16ebc981f56a6efe57d8edef0edbb0898
Teo <teodorbogoeski@gmail.com>
2022-06-27 17:33:28 +0200

    update post_form and add json file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Form
-from starlette.responses import FileResponse 
+from starlette.responses import FileResponse
+
+from utils import load_reviews, sort_reviews, filter_reviews
 
 app = FastAPI()
 
@@ -8,8 +10,27 @@ async def index():
     return FileResponse('index.html')
 
 @app.post("/post_form")
-async def post_form(orderByRatingHighest: bool = Form(),
-                    orderByDateNewest: bool = Form(),
-                    prioritizeByText: bool = Form(),
-                    minimumRating: int = Form()):
-    return (orderByRatingHighest, orderByDateNewest, prioritizeByText, minimumRating)
+async def post_form(
+        orderByRatingHighest: bool = Form(),
+        orderByDateNewest: bool = Form(),
+        prioritizeByText: bool = Form(),
+        minimumRating: int = Form()
+    ):
+
+    """
+    Returns a json file with the reviews that match the parameters
+    """
+    raw_reviews, reviews = load_reviews()
+    reviews_ids = [review['id'] for review in raw_reviews]
+    result = []
+
+    # filter reviews
+    reviews = sort_reviews(reviews, prioritizeByText, orderByDateNewest, orderByRatingHighest)
+    filtered_ids = filter_reviews(reviews, minimumRating)
+
+    # find the reviews that match the filtered ids
+    for id in filtered_ids:
+        if id in reviews_ids:
+            result.append(raw_reviews[reviews_ids.index(id)])
+
+    return result
